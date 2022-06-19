616f5cfce60f9a85c8cc49d44c4b681a13e4b090
Victoria Kozhevnikova <victoria_kozhevnikova@epam.com>
2022-06-19 16:28:31 +0300

    FilterByTags component

---
+++ src/components/FilterByTags/FilterByTags.tsx
@@ -0,0 +1,20 @@
+import * as React from 'react';
+import cn from 'classnames';
+import { IFilterByTagsProps } from './IFilterByTagsProps';
+
+import styles from './FilterByTags.module.scss';
+
+export const FilterByTags: React.FC<IFilterByTagsProps> = ({ options }) => {
+  return (
+    <div className={styles.filter}>
+      <button className={cn(styles.button, styles.isActive)} type='button'>
+        All
+      </button>
+      {Object.keys(options).map(item => (
+        <button key={item} className={styles.button} type='button'>
+          {options[item]}
+        </button>
+      ))}
+    </div>
+  );
+};
---
+++ src/components/FilterByTags/index.ts
@@ -0,0 +1,1 @@
+export { FilterByTags } from './FilterByTags';
