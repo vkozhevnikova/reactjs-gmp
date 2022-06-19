59b91aac5ad756bd86b947fd0fe14159152e62fe
Victoria Kozhevnikova <victoria_kozhevnikova@epam.com>
2022-06-19 16:25:51 +0300

    MovieList component

---
+++ src/components/MovieList/MovieList.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import { IMovieListProps } from './IMovieListProps';
+import { MovieCard } from '../MovieCard';
+
+import styles from './MovieList.module.scss';
+
+export const MovieList: React.FC<IMovieListProps> = ({ movies }) => {
+  return (
+    <div className={styles.list}>
+      {movies.map(movie => (
+        <MovieCard key={movie.id} movie={movie} className={styles.card} />
+      ))}
+    </div>
+  );
+};
