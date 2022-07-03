9bd90bc60dce5a75664e3bbc484ad234a07caa99
Victoria Kozhevnikova <victoria_kozhevnikova@epam.com>
2022-07-03 12:51:45 +0300

    created Modal component

---
+++ src/components/UI/Modal/Modal.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { IModalProps } from './interfaces';
+import { Icon } from '../Icon';
+
+import styles from './Modal.module.scss';
+
+import closeIcon from '../../../assets/sprites/close.svg';
+
+const modalRoot = document.getElementById('modal-root');
+
+export const Modal: React.FC<IModalProps> = ({ children, isOpen, handlerModal }) => {
+  if (!isOpen) return null;
+
+  const onClose = () => {
+    document.body.classList.remove('overflow');
+    handlerModal(false);
+  };
+
+  return ReactDOM.createPortal(
+    <>
+      <div className={styles.overlay} />
+
+      <div className={styles.modal}>
+        <div className={styles.wrapper}>
+          <div className={styles.container}>
+            <button className={styles.close} type='button' onClick={onClose}>
+              <Icon icon={closeIcon} />
+            </button>
+
+            {children}
+          </div>
+        </div>
+      </div>
+    </>,
+    modalRoot,
+  );
+};
