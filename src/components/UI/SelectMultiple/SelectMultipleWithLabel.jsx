ff9f2d0ed2eeefca221e4a62b35deb6f46e4a5c6
Victoria Kozhevnikova <victoria_kozhevnikova@epam.com>
2022-07-03 12:50:29 +0300

    added SelectMultiple component

---
+++ src/components/UI/SelectMultiple/SelectMultipleWithLabel.jsx
@@ -0,0 +1,4 @@
+import { makeFieldWithLabel } from '../../hoc/makeFieldWithLabel';
+import { SelectMultiple } from './SelectMultiple';
+
+export const SelectMultipleWithLabel = makeFieldWithLabel(SelectMultiple);
